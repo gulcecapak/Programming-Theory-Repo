94f8c188fe10b711d9658b1622a97979437c8028
Gulce Capak <gulcecapak@gmail.com>
2023-06-14 11:05:46 +0300

    Final

--- Assets/Scripts/BallSpawn.cs
+++ Assets/Scripts/BallSpawn.cs
@@ -25,7 +25,7 @@ public class BallSpawn : MonoBehaviour
 
         Instantiate(ballPrefabs[ballIndex], GenerateSpawnPosition(), ballPrefabs[ballIndex].transform.rotation);
 
-        float spawnInterval = 1.0f;
+        float spawnInterval = 2.0f;
 
         Invoke("SpawnBall", spawnInterval);
     }
